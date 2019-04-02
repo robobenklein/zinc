521dd0f850c7fd471736cbb78dff815ca654a6cc
Ben Klein <robobenklein@gmail.com>
2019-04-02 13:20:44 -0400

    Virtualenv segment cuz I need it

--- presets/robobenklein.zincp.zsh
+++ presets/robobenklein.zincp.zsh
@@ -4,7 +4,7 @@ zinc_left=(
 )
 
 zinc_right=(
-  zincs_retval zincs_execution_time zincs_jobs zincs_battery zincs_time
+  zincs_retval zincs_execution_time zincs_jobs zincs_battery zincs_time zincs_virtualenv
 )
 
 typeset -gA zincs_cwd
