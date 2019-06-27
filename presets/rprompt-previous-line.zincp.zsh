9b633341d0e802afb8438c7fde3a24b616d03db8
Ben Klein <robobenklein@gmail.com>
2019-06-27 15:24:25 -0400

    Testing new strat for rprompt newline

--- presets/rprompt-previous-line.zincp.zsh
+++ presets/rprompt-previous-line.zincp.zsh
@@ -3,25 +3,26 @@ function zc_begin () {
   REPLY="%{" # invis chars
   # REPLY+=$'\033[s' # save curpos
   REPLY+=$'\033[1A' # up 1L
-  REPLY+=$'\033[1C' # fwd over segment join space
+  # REPLY+=$'\033[1C' # fwd over segment join space
   REPLY+="%}"
 }
 
 function zc_end () {
-  REPLY=" %{" # invis chars
-  # REPLY+=$'\033[u' # restore curpos
+  REPLY=""
+  REPLY+="${_ZINC_SEP_CHARS[rprompt_terminating_space]}"
+  REPLY+="%{" # invis chars
   # REPLY+=$'\033[1D' # backspace over segment join space
   REPLY+=$'\033[1B' # down 1L
+  # REPLY+=$'\033[u' # restore curpos
   REPLY+="%}"
 }
 
 function a_newline () {
-  REPLY="
-"
+  REPLY=$'\n'
 }
 
 zinc_left=(
-  a_newline
+  # a_newline
   zincs_userhost zincs_cwd_writable zincs_cwd zincs_vcs
 )
 
@@ -30,6 +31,12 @@ zinc_right=(
   zincs_retval zincs_execution_time zincs_jobs zincs_battery zincs_time zincs_virtualenv
   zc_end
 )
+
+function _zc_newline () {
+  echo ""
+}
+
+-zinc-add-hook _zinc_precmd _zc_newline
 
 zinc_opts=(
   a_newline "none;none;ZINC_SEGMENT_NONRENDERABLE;none"
